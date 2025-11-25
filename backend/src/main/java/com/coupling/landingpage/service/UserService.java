e32dd54bf715c1f6d4845f9822e39a4564b02727
auhe <adrian.uhe@uni-bielefeld.de>
2025-11-25 20:41:33 +0100

    Deleted Backend

--- backend/src/main/java/com/coupling/landingpage/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.coupling.landingpage.service;
-
-import com.coupling.landingpage.model.User;
-import java.util.List;
-
-public interface UserService {
-    User saveUser(User user);
-    List<User> getAllUsers();
-}
